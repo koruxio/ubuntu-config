ce5325cdef01db926722e01a5b6394eeba5bb9f4
sarosx <devv.machine01@protonmail.com>
2022-05-15 22:53:05 +0530

    added nerd font

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-cp -r .themes .config $HOME
+cp -r .themes .config .fonts $HOME
 
 # To export custom shortcuts
 # dconf dump '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' > custom-keybindings.dconf
@@ -6,3 +6,4 @@ cp -r .themes .config $HOME
 # To import custom shortcuts
 
 dconf load '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' < custom-keybindings.dconf
+fc-cache -fv
